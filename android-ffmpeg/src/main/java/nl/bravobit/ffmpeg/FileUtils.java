c4847e50b9ebcc8a4f31a1e07a75bc9bfea9c9a9
HiroyukTamura <hirotamu3@gmail.com>
2019-10-01 17:40:54 +0900

    ffprobeをライブラリから削除

--- android-ffmpeg/src/main/java/nl/bravobit/ffmpeg/FileUtils.java
+++ android-ffmpeg/src/main/java/nl/bravobit/ffmpeg/FileUtils.java
@@ -2,24 +2,16 @@ package nl.bravobit.ffmpeg;
 
 import android.content.Context;
 
-import java.io.BufferedInputStream;
+import androidx.annotation.NonNull;
+
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 class FileUtils {
     private static final String FFMPEG_FILE_NAME = "lib..ffmpeg..so";
-    private static final String FFPROBE_FILE_NAME = "lib..ffprobe.so";
 
-    static File getFFmpeg(Context context) {
+    @NonNull
+    static File getFFmpeg(@NonNull Context context) {
         File folder = new File(context.getApplicationInfo().nativeLibraryDir);
         return new File(folder, FFMPEG_FILE_NAME);
-    }
-
-    static File getFFprobe(Context context) {
-        File folder = new File(context.getApplicationInfo().nativeLibraryDir);
-        return new File(folder, FFPROBE_FILE_NAME);
     }
 }
